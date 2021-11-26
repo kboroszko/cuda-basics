ad7574605b1a963f92887e67644365a7b337ac82
Kajetan Boroszko <kajetan@unoperate.com>
2021-11-26 11:18:54 +0100

    initial commit

---
+++ matmul/mat_mul.cu
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "common/errors.h"
+
+#define N 1000
+
+__global__ void add(int *a, int *b, int *c) {
+    int bid = blockIdx.x;
+	int tid = bid * 256 + threadIdx.x;
+	if (tid < N) {
+		c[tid] = a[tid] + b[tid];
+    }
+}
+
+int main(void) {
+	int a[N], b[N], c[N];
+	int *devA, *devB, *devC;
+
+	HANDLE_ERROR(cudaMalloc((void**)&devA, N * sizeof(int)));
+	HANDLE_ERROR(cudaMalloc((void**)&devB, N * sizeof(int)));
+	HANDLE_ERROR(cudaMalloc((void**)&devC, N * sizeof(int)));
+
+	//fill a and b arrays
+    for(int i=0; i<N; i++){
+        a[i] = i;
+        b[i] = i*2;
+    }
+
+	HANDLE_ERROR(cudaMemcpy(devA, a, N * sizeof(int), cudaMemcpyHostToDevice));
+	HANDLE_ERRORcudaMemcpy(devB, b, N * sizeof(int), cudaMemcpyHostToDevice));
+	add<<<(N+255)/256,256>>>(devA, devB, devC);
+	HANDLE_ERRORcudaMemcpy(c, devC, N * sizeof(int), cudaMemcpyDeviceToHost));
+
+
+	//check if ok
+    for(int i=0; i<N; i++){
+        if(c[i] != a[i] + b[i]){
+            printf("error at cell %d, %d != %d", i, c[i], a[i]+b[i]);
+            return 1;
+        }
+    }
+
+	HANDLE_ERRORcudaFree(devA));
+	HANDLE_ERRORcudaFree(devB));
+	HANDLE_ERRORcudaFree(devC));
+	return 0;
+}
